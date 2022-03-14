dd00e4c7dd6f2dede96437827c31701fc348aeab
Fernando Dingler <ferdingler@users.noreply.github.com>
2022-03-14 10:18:38 -0700

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "remix";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Bienvenido a Remix</h1>
       <ul>
         <li>
           <a
